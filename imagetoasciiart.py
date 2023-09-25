5aa42604d3530cdb3a591f9f9cb5a0d2211e49dd
mickr777 <115216705+mickr777@users.noreply.github.com>
2023-09-25 15:49:44 +1000

    Update imagetoasciiart.py

--- imagetoasciiart.py
+++ imagetoasciiart.py
@@ -3,6 +3,7 @@ from invokeai.app.invocations.baseinvocation import BaseInvocation, InvocationCo
 from invokeai.app.invocations.primitives import ImageField, ImageOutput
 from invokeai.app.models.image import ImageCategory, ResourceOrigin
 
+
 @invocation(
     "Image_to_ASCII_Art_Image",
     title="Image to ASCII Art Image",
@@ -11,19 +12,24 @@ from invokeai.app.models.image import ImageCategory, ResourceOrigin
     version="0.5.0",
 )
 class ImageToDetailedASCIIArtInvocation(BaseInvocation):
-    input_image: ImageField = InputField(description="Input image to convert to detailed ASCII art")
-    font_spacing: int = InputField(default=6, description="Font size for the ASCII art characters")
-    detailed_mode: bool = InputField(default=True, description="Enable detailed mode (default: true)")
-    color_mode: bool = InputField(default=False, description="Enable color mode (default: grayscale)")
+    input_image: ImageField = InputField(
+        description="Input image to convert to detailed ASCII art")
+    font_spacing: int = InputField(
+        default=6, description="Font size for the ASCII art characters")
+    detailed_mode: bool = InputField(
+        default=True, description="Enable detailed mode (default: true)")
+    color_mode: bool = InputField(
+        default=False, description="Enable color mode (default: grayscale)")
 
     def image_to_detailed_ascii_art(self, input_image: Image.Image, font_spacing: int, detailed_mode: bool, color_mode: bool) -> Image.Image:
         if detailed_mode:
-            ascii_chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\^`'. "
+            ascii_chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\\^`'. "
         else:
-            ascii_chars = ".:-=+*#%@"
+            ascii_chars = "@%#*+=-:. "
 
         if color_mode:
-            ascii_art_image = Image.new("RGB", input_image.size, (255, 255, 255))
+            ascii_art_image = Image.new(
+                "RGB", input_image.size, (255, 255, 255))
         else:
             ascii_art_image = Image.new("L", input_image.size, 255)
 
@@ -34,7 +40,8 @@ class ImageToDetailedASCIIArtInvocation(BaseInvocation):
 
         for y in range(num_rows):
             for x in range(num_cols):
-                pixel_value = input_image.getpixel((x * font_spacing, y * font_spacing))
+                pixel_value = input_image.getpixel(
+                    (x * font_spacing, y * font_spacing))
 
                 if isinstance(pixel_value, tuple):
                     pixel_value = pixel_value[0]
@@ -44,16 +51,22 @@ class ImageToDetailedASCIIArtInvocation(BaseInvocation):
                 ascii_char = ascii_chars[ascii_index]
 
                 if color_mode:
-                    color = input_image.getpixel((x * font_spacing, y * font_spacing))
-                    draw.text((x * font_spacing, y * font_spacing), ascii_char, fill=color)
+                    color = input_image.getpixel(
+                        (x * font_spacing, y * font_spacing))
+                    draw.text((x * font_spacing, y * font_spacing),
+                              ascii_char, fill=color)
                 else:
-                    draw.text((x * font_spacing, y * font_spacing), ascii_char, fill=int(pixel_value))
+                    # Fill is black, and the character itself represents the shade
+                    draw.text((x * font_spacing, y * font_spacing),
+                              ascii_char, fill=0)
 
         return ascii_art_image
 
     def invoke(self, context: InvocationContext) -> ImageOutput:
-        input_image = context.services.images.get_pil_image(self.input_image.image_name)
-        detailed_ascii_art_image = self.image_to_detailed_ascii_art(input_image, self.font_spacing, self.detailed_mode, self.color_mode)
+        input_image = context.services.images.get_pil_image(
+            self.input_image.image_name)
+        detailed_ascii_art_image = self.image_to_detailed_ascii_art(
+            input_image, self.font_spacing, self.detailed_mode, self.color_mode)
         mask_dto = context.services.images.create(
             image=detailed_ascii_art_image,
             image_origin=ResourceOrigin.INTERNAL,
