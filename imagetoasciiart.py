ce8d5a8ae91df5b3cd9c130ad19bc73750f476bd
mickr777 <115216705+mickr777@users.noreply.github.com>
2024-03-12 20:27:35 +1100

    Updated for v4.0

--- imagetoasciiart.py
+++ imagetoasciiart.py
@@ -1,31 +1,26 @@
 import os
 from typing import Literal, Optional
 from PIL import Image, ImageDraw
-from invokeai.app.invocations.baseinvocation import (
+from invokeai.invocation_api import (
     BaseInvocation,
-    Input,
     InvocationContext,
     invocation,
     InputField,
     WithMetadata,
+    ImageField, 
+    ImageOutput, 
 )
-from invokeai.app.invocations.primitives import ImageField, ImageOutput, BoardField
-from invokeai.app.services.image_records.image_records_common import (
-    ImageCategory,
-    ResourceOrigin,
-)
-
 
 @invocation(
     "Image_to_ASCII_Art_Image",
     title="Image to ASCII Art Image",
     tags=["image", "ascii art"],
     category="image",
-    version="1.3.6",
+    version="1.4.0",
     use_cache=False,
 )
 class ImageToDetailedASCIIArtInvocation(BaseInvocation, WithMetadata):
-    """Convert an Image to Ascii Art Image"""
+    """Convert an Image to ASCII Art Image"""
 
     input_image: ImageField = InputField(
         description="Input image to convert to ASCII art"
@@ -49,9 +44,6 @@ class ImageToDetailedASCIIArtInvocation(BaseInvocation, WithMetadata):
     )
     gamma: float = InputField(
         default=1.0, description="Gamma correction value for the output image"
-    )
-    board: Optional[BoardField] = InputField(
-        default=None, description="Pick Board to add output too", input=Input.Direct
     )
 
     def get_ascii_chars(self):
@@ -173,7 +165,7 @@ class ImageToDetailedASCIIArtInvocation(BaseInvocation, WithMetadata):
         return directory_path
 
     def invoke(self, context: InvocationContext) -> ImageOutput:
-        input_image = context.services.images.get_pil_image(self.input_image.image_name)
+        input_image = context.images.get_pil(self.input_image.image_name)
 
         if self.output_to_file:
             ascii_str = self.image_to_ascii_string(input_image, self.font_spacing)
@@ -187,20 +179,6 @@ class ImageToDetailedASCIIArtInvocation(BaseInvocation, WithMetadata):
         detailed_ascii_art_image = self.image_to_detailed_ascii_art(
             input_image, self.font_spacing, self.color_mode
         )
-        mask_dto = context.services.images.create(
-            image=detailed_ascii_art_image,
-            image_origin=ResourceOrigin.INTERNAL,
-            image_category=ImageCategory.GENERAL,
-            board_id=self.board.board_id if self.board else None,
-            node_id=self.id,
-            session_id=context.graph_execution_state_id,
-            is_intermediate=self.is_intermediate,
-            metadata=self.metadata,
-            workflow=context.workflow,
-        )
+        image_dto = context.images.save(image=detailed_ascii_art_image)
 
-        return ImageOutput(
-            image=ImageField(image_name=mask_dto.image_name),
-            width=mask_dto.width,
-            height=mask_dto.height,
-        )
+        return ImageOutput.build(image_dto)
